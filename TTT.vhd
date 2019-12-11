1609ba4b8f85e2ecba3241030bee80a527c13689
kenysanchez <kenya.gaby@hotmail.com>
2019-12-10 23:45:30 -0600

    Add files via upload

---
+++ TTT.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:28:18 12/10/2019 
+-- Design Name: 
+-- Module Name:    TTT - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TTT is
+end TTT;
+
+architecture Behavioral of TTT is
+
+begin
+
+
+end Behavioral;
+
